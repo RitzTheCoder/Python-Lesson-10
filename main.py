9437c38b28530f555533db04c7e89dd51389729c
RitzTheCoder <ritzthecoder@gmail.com>
2025-04-04 17:32:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+string = input("please enter your own word :")
+
+char = input("please enter your own character :")
+
+i = 0
+
+count = 0
+
+while i < len(string):
+
+ if(string[i] == char):
+
+ count = count + 1
+
+
+i = i + 1
+
+print("The number of times the character appears in the string is : ", count)
